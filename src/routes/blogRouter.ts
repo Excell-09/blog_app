48bde367e23c5b4c4b9bfe09ccc0e4c128ee3b5c
Excell <excell.agcb12@gmail.com>
2024-03-07 23:43:34 +0700

    add authentication upload router

--- src/routes/blogRouter.ts
+++ src/routes/blogRouter.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import upload from "../config/multer";
 import { uploadBanner } from "../controllers/blog";
+import authentication from "../middleware/authentication";
 
 const router = Router();
 
-router.post("/upload/banner", upload.single("banner"), uploadBanner);
+router.post(
+  "/upload/banner",
+  authentication(),
+  upload.single("banner"),
+  uploadBanner
+);
 
 export default router;
