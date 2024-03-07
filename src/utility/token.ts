229c5558b63f1ed999c4e28a87d3c6eefc8f4026
Excell <excell.agcb12@gmail.com>
2024-03-07 22:05:37 +0700

    delete env config exp refresh

--- src/utility/token.ts
+++ src/utility/token.ts
@@ -1,9 +1,10 @@
 import jwt from "jsonwebtoken";
 import { opts } from "../controllers/authenticationControllers";
+import AUTHCONFIG from "../config/auth";
 
 function generateAccessToken(userId: string) {
   return jwt.sign({ id: userId }, opts.secretOrKey, {
-    expiresIn: "1m",
+    expiresIn: AUTHCONFIG.expired_access_token,
   });
 }
 
