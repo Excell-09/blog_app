a2ad9a6e80e9b7328a7213b684e18013cc17150e
Excell <excell.agcb12@gmail.com>
2024-03-08 16:26:29 +0700

    set up layot

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,40 @@
-import { Button } from "./components/ui/button";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
+import SignUp from "./pages/sign-up/SignUp";
+import SignIn from "./pages/sign-in/SignIn";
+import Layout from "./pages/Layout";
+import Blog from "./pages/blog/Blog";
+
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/blog/:id",
+        Component: Blog,
+      },
+      {
+        path: "/signin",
+        Component: SignIn,
+      },
+      {
+        path: "/signup",
+        Component: SignUp,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <Button>Haii</Button>;
+  return (
+    <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
+  );
 }
 
 export default App;
